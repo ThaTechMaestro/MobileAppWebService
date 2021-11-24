3f34b7ff1f54e85ad7aa5dcd2ee53092efc91f3d
ThaTechMaestro <muse.pearls@gmail.com>
2021-11-24 08:20:28 +0100

    Initial commit

---
+++ new_maws/src/test/java/com/project/mobileappwebservice/MobileappwebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mobileappwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileappwebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
